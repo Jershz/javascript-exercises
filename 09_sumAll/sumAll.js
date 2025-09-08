17be8dc53324c904d7f7011eaaf7442552e7f09d
Jershz <joshuavasher@comcast.net>
2025-09-08 09:19:51 -0400

    sumAll.js: Complete exercise

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if(!Number.isInteger(a+b)) return "ERROR";
+    let sum = 0;
+    let start = a;
+    let end = b;
+    if(a>b && a-b > 0) {        
+        start = b;
+        end = a;
+    }
 
+    for(let i = start; i <= end; i++) {
+        sum += i;
+    }
+    return sum > 0 ? sum : "ERROR";
 };
 
 // Do not edit below this line
