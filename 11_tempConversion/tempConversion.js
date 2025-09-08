0d76d36a9b1f41b96923ab055023c8f914f5322b
Jershz <joshuavasher@comcast.net>
2025-09-08 09:40:06 -0400

    tempConversion.js: Complete exercise

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+function roundToOneDecimalPlace(num) {
+  return Math.round(num * 10) / 10;
+}
+
+const convertToCelsius = function(temp) {
+  return roundToOneDecimalPlace(((temp - 32) * 5) / 9);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  return roundToOneDecimalPlace(((temp * 9) / 5) + 32);
 };
 
 // Do not edit below this line
