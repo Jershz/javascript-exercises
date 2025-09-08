e010b1b0b0f03eb8aaddcc2b40580eb0a75fba21
Jershz <joshuavasher@comcast.net>
2025-09-08 09:29:13 -0400

    leapYears.js: Complete exercise

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    //if year divided by 4 has no remainder
+    //and year divided by 100 has a remainder
+    //return true
+    //if year divided by 4 has no remainder
+    //and year divided by 100 has no remainder
+    //return true
 
+    if(year % 4 === 0 && year % 100 !== 0) return true;
+    if(year % 4 === 0 && year % 100 === 0 && year % 400 === 0) return true;
+    return false;
 };
 
 // Do not edit below this line
