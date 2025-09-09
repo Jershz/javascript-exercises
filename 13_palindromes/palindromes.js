7caca70836e7f9cbc1de9640d37e9fa03757ce71
Jershz <joshuavasher@comcast.net>
2025-09-09 14:23:30 -0400

    palindromes.js: Complete exercise

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
-
+const palindromes = function (str = "") {
+    str = str.toLowerCase();
+    
+    let arr = Array.from(str)
+            .filter(char => char !== " " && char !== "." && char !== "!" && char !== ",")
+            .join('');
+    
+    let arrReversed = arr.split('').reverse().join('');
+    
+    for(let i = 0; i < arr.length; i++) {
+        console.log(arr[i] + " " + arrReversed[i]);
+        if(arr[i] !== arrReversed[i]) return false;
+    }
+    return true;
 };
 
 // Do not edit below this line
