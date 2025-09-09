629b8fb03fe96a13dee7008886fa2ffd8d570168
Jershz <joshuavasher@comcast.net>
2025-09-09 13:59:10 -0400

    calculator.js: Complete exercise

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(a,b) {
+  return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let sum = 0;
+	for(let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((total,curr) => total * curr, 1);
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return Math.pow(a,b);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  if(a == 0) return 1;
+  for(let i = a-1; i > 0; i--) {
+    a *= i;
+  }
+  return a;
 };
 
 // Do not edit below this line
