f8bd7a36982f08646034c8a72c71e400b5152f7d
Jershz <joshuavasher@comcast.net>
2025-09-09 14:48:28 -0400

    findTheOldest.js: Complete exercise

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(objArr) {
+    const currentDate = new Date();
+    const currentYear = currentDate.getFullYear();
+    return objArr.reduce((oldest, current) => {
+        if(current.yearOfDeath === undefined) current.yearOfDeath = currentYear;
+        if(oldest.yearOfDeath === undefined) oldest.yearOfDeath = currentYear;
+        return (current.yearOfDeath - current.yearOfBirth) > (oldest.yearOfDeath - oldest.yearOfBirth) ? current : oldest;
+    }, objArr[0]);
 };
 
 // Do not edit below this line
