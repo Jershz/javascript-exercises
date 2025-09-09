ee0800f281050d1c4b1c4f1c3f2a6537d86a51af
Jershz <joshuavasher@comcast.net>
2025-09-09 14:34:27 -0400

    fibonacci.js: Complete exercise:

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function(seq) {
+    let arr = [0, 1];
+    if(seq < 0) return "OOPS";
+    seq = parseInt(seq);
+    for(let i = 1; i < seq; i++ ) {
+        arr.push(arr[i]+(arr[i-1]));
+    }   
+    return arr[seq];
 };
 
 // Do not edit below this line
